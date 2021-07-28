a2ac317091b9d0a776325c25824d800f44ff4468
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-07-28 10:52:58 -0500

    tried new things to render my entries, not working yet

--- scripts/Entries.js
+++ scripts/Entries.js
@@ -12,39 +12,9 @@ export const Entries = () => {
 
     for (const entry of entries) {
         allEntriesAsHTML += `
-        <form class="entryForm">
-        <fieldset>
-            <label for="entryDate">Date</label>
-            <input type="date" name="entryDate" class="entryForm__date">
-        </fieldset>
 
-        <fieldset>
-            <label for="entryConcepts">Concepts covered</label>
-            <input type="text" name="entryConcepts" class="entryForm__concepts">
-        </fieldset>
-        
-        <fieldset>
-            <label for="entryJournalEntry">Journal Entry</label>
-            <textarea id="entryJournalEntry" name="entryJournalEntry" class="entryForm_entry">Write your journal entry here</textarea>
-        </fieldset>
-
-        <fieldset>
-            <label for="entryMood">Mood for the day</label>
-            <select>
-                <option>Happy</option>
-                <option>Sad</option>
-                <option>In pain</option>
-                <option>Excited</option>
-                <option>Tired</option>
-                <option>Melty Brain</option>
-            </select>
-        </fieldset>
-
-        <button type="button">Record Journal Entry</button>
-    </form>
         `
     }
     
     return allEntriesAsHTML
-    console.log(allEntriesAsHTML)
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 import { DailyJournal } from "./DailyJournal.js"
 
-const dailyJournal = DailyJournal()
 const container = document.querySelector("#entries")
 
 const render = () => {
     container.innerHTML = DailyJournal()
 }
+
+render ()
