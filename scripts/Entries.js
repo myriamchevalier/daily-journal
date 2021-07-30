ada33959801f4d2c1cee8f15d90e608618c795f9
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-07-30 15:13:34 -0500

    rendered html containing journal entries. Needs work

--- scripts/Entries.js
+++ scripts/Entries.js
@@ -12,39 +12,21 @@ export const Entries = () => {
 
     for (const entry of entries) {
         allEntriesAsHTML += `
-        <form class="entryForm">
-        <fieldset>
-            <label for="entryDate">Date</label>
-            <input type="date" name="entryDate" class="entryForm__date">
-        </fieldset>
-
-        <fieldset>
-            <label for="entryConcepts">Concepts covered</label>
-            <input type="text" name="entryConcepts" class="entryForm__concepts">
-        </fieldset>
         
-        <fieldset>
-            <label for="entryJournalEntry">Journal Entry</label>
-            <textarea id="entryJournalEntry" name="entryJournalEntry" class="entryForm_entry">Write your journal entry here</textarea>
-        </fieldset>
+        <h3>Date</h3>
+        ${entry.date}
+        
+        <h3>Concept</h3>
+        ${entry.concept}
 
-        <fieldset>
-            <label for="entryMood">Mood for the day</label>
-            <select>
-                <option>Happy</option>
-                <option>Sad</option>
-                <option>In pain</option>
-                <option>Excited</option>
-                <option>Tired</option>
-                <option>Melty Brain</option>
-            </select>
-        </fieldset>
+        <h3>Journal Entry</h3>
+        ${entry.entry}
 
-        <button type="button">Record Journal Entry</button>
-    </form>
+        <h3>Mood</h3>
+        ${entry.mood}
+
         `
     }
     
     return allEntriesAsHTML
-    console.log(allEntriesAsHTML)
 }
